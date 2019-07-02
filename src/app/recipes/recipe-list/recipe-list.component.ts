4131edcf1bb13e8ad5673177a4bd15a2cf3187e8
TopGitSan <c.toparcean@gmail.com>
2019-07-02 20:39:43 +0200

    add recipe-list html and functionality

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Recipe } from "../recipe.model";
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  selector: "app-recipe-list",
+  templateUrl: "./recipe-list.component.html",
+  styleUrls: ["./recipe-list.component.css"]
 })
 export class RecipeListComponent implements OnInit {
+  shadow: string;
+  recipes: Recipe[] = [
+    new Recipe(
+      "Lasagna",
+      "Meat and pasta. Bologna's favorit dish",
+      "http://assets.kraftfoods.com/recipe_images/opendeploy/Table-for-Two_Lasagna_640x428.jpg"
+    ),
+    new Recipe(
+      "Lasagna",
+      "Meat and pasta. Bologna's favorit dish",
+      "http://assets.kraftfoods.com/recipe_images/opendeploy/Table-for-Two_Lasagna_640x428.jpg"
+    )
+  ];
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  changeStyle($event){
+    this.shadow = $event.type == 'mouseover' ? '0 0 6px grey' : 'none';
+    // this.shadow = $event.type == 'mouseout' ? 'none' : '0 0 6px grey';
   }
-
 }
