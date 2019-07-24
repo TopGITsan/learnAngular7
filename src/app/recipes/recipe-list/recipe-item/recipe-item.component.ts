95e0463cfe72aa14c80aa56a138af7f2a2327442
TopGitSan <c.toparcean@gmail.com>
2019-07-24 23:07:24 +0200

    use service for cross-comp communication

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,27 +1,27 @@
-import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+import { Component, OnInit, Input } from '@angular/core';
 
-import { Recipe } from "../../recipe.model";
+import { Recipe } from '../../recipe.model';
+import { RecipeService } from '../../recipe.service';
 
 @Component({
-  selector: "app-recipe-item",
-  templateUrl: "./recipe-item.component.html",
-  styleUrls: ["./recipe-item.component.css"]
+  selector: 'app-recipe-item',
+  templateUrl: './recipe-item.component.html',
+  styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
   shadow: string;
-  @Output() recipeSelected = new EventEmitter<void>();
 
-  constructor() {}
+  constructor(private recipeService: RecipeService) {}
 
   ngOnInit() {}
 
   changeStyle($event) {
-    this.shadow = $event.type == "mouseover" ? "0 0 6px grey" : "none";
+    this.shadow = $event.type == 'mouseover' ? '0 0 6px grey' : 'none';
     // this.shadow = $event.type == 'mouseout' ? 'none' : '0 0 6px grey';
   }
 
   onSelected() {
-    this.recipeSelected.emit();
+    this.recipeService.recipeSelected.emit(this.recipe);
   }
 }
