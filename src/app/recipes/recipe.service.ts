1a856d7cdabe304c28b5125e1158f8d964c9a690
TopGitSan <c.toparcean@gmail.com>
2019-07-29 20:30:24 +0200

    add ingredients to the recipe model

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,7 @@
 import { EventEmitter } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
@@ -8,13 +9,15 @@ export class RecipeService {
   private recipes: Recipe[] = [
     new Recipe(
       'Lasagna',
-      'Meat and pasta. Bologna\'s favorit dish',
-      'http://assets.kraftfoods.com/recipe_images/opendeploy/Table-for-Two_Lasagna_640x428.jpg'
+      'Meat and pasta. Roma\'s not so favorit dish',
+      'http://assets.kraftfoods.com/recipe_images/opendeploy/Table-for-Two_Lasagna_640x428.jpg',
+      [new Ingredient('Meat', 4), new Ingredient('Mozzarella', 2)]
     ),
     new Recipe(
       'Lasagna Bolognese',
       'Meat and pasta and some. Bologna\'s favorit dish',
-      'http://assets.kraftfoods.com/recipe_images/opendeploy/Table-for-Two_Lasagna_640x428.jpg'
+      'http://assets.kraftfoods.com/recipe_images/opendeploy/Table-for-Two_Lasagna_640x428.jpg',
+      [new Ingredient('Meat', 8), new Ingredient('Mozzarella', 6)]
     )
   ];
 
