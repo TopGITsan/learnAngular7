89f660e22d1f6d89b4e4f8c23833e8d77265a989
TopGitSan <c.toparcean@gmail.com>
2019-09-08 22:29:46 +0200

    clean unused code

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,4 @@
-import { EventEmitter, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
@@ -8,7 +8,6 @@ import { Subject } from 'rxjs';
 
 @Injectable()
 export class RecipeService {
-  recipeSelected = new EventEmitter<Recipe>();
 
   recipeActivate = new Subject();
 
