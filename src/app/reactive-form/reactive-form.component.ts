df141af93d2306bcda46bbac41c7761e3886aca0
TopGitSan <c.toparcean@gmail.com>
2019-09-19 23:29:01 +0200

    create a custom validator

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -9,12 +9,13 @@ import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 export class ReactiveFormComponent implements OnInit {
   signupForm: FormGroup;
   genders = ['male', 'female'];
+  forbbidenNames = ['noob','robot', 'Scorpion', 'bot'];
   constructor() { }
 
   ngOnInit() {
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required),
+        'username': new FormControl(null, [Validators.required, this.checkForbbidenName.bind(this)]),
         'email': new FormControl(null, [Validators.required, Validators.email]),
       }),
       'gender': new FormControl('male'),
@@ -24,11 +25,19 @@ export class ReactiveFormComponent implements OnInit {
 
   onSubmit(){
     console.log(this.signupForm);
+    this.signupForm.reset();
   }
 
   onAddHobbie(){
     const control = new FormControl(null, Validators.required);
     (this.signupForm.get('hobbies') as FormArray).push(control);
+  }
+
+  checkForbbidenName(control: FormControl):{[s: string]: boolean}{
+    if (this.forbbidenNames.indexOf(control.value) !== -1) {
+      return {'forbbidenName': true}
+    }
+    return null;
   }
 
 }
