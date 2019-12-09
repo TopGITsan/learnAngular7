0faa4fe86cfe739912214c29df4a2eac1678dd05
TopGitSan <c.toparcean@gmail.com>
2019-12-09 22:28:15 +0100

    add the TemplateDriven shopping list form

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 import { Ingredient } from 'src/app/shared/ingredient.model';
 
 import { ShoppingListService } from '../shopping-list.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -10,17 +11,14 @@ import { ShoppingListService } from '../shopping-list.service';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-  @ViewChild('nameInput') nameInputRef: ElementRef;
-  @ViewChild('amountInput') amountInputRef: ElementRef;
 
   constructor(private slService: ShoppingListService) {}
 
   ngOnInit() {}
 
-  onAddItem() {
-    const ingName = this.nameInputRef.nativeElement.value;
-    const amountName = this.amountInputRef.nativeElement.value;
-    const newIngredient = new Ingredient(ingName, amountName);
+  onAddItem(form: NgForm) {
+    const value = form.value;
+    const newIngredient = new Ingredient(value.name, value.amount);
     this.slService.addIngredient(newIngredient);
   }
 }
