6fb36ffccf62ca90132269bf3d0fab9459cf24cd
TopGitSan <c.toparcean@gmail.com>
2019-12-14 01:00:59 +0100

    validate user input

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -58,17 +58,17 @@ export class RecipeEditComponent implements OnInit, OnDestroy {
         for (let ingredient of recipe.ingredients) {
           recipeIngredients.push(
             new FormGroup({
-              'name': new FormControl(ingredient.name),
-              'amount': new FormControl(ingredient.amount)
+              'name': new FormControl(ingredient.name, Validators.required),
+              'amount': new FormControl(ingredient.amount,[Validators.required, Validators.pattern(/^[1-9]+[0-9]*$/)])
             })
           );
         }
       }
     }
     this.recipeForm = new FormGroup({
-      'name': new FormControl(recipeName),
-      'imagePath': new FormControl(recipeImagePath),
-      'description': new FormControl(recipeDescription),
+      'name': new FormControl(recipeName, Validators.required),
+      'imagePath': new FormControl(recipeImagePath, Validators.required),
+      'description': new FormControl(recipeDescription, Validators.required),
       'ingredients': recipeIngredients
     });
   }
@@ -80,8 +80,8 @@ export class RecipeEditComponent implements OnInit, OnDestroy {
   onAddIngredient(){
     (<FormArray>this.recipeForm.get('ingredients')).insert(0,
       new FormGroup({
-        'name': new FormControl('', Validators.required),
-        'amount': new FormControl('', Validators.required)
+        'name': new FormControl(null, Validators.required),
+        'amount': new FormControl(null, [Validators.required, Validators.pattern(/^[1-9]+[0-9]*$/)])
       })
     )
   }
