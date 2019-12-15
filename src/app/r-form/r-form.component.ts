18f39bf0f05dccdc082fb160c99e5fd9dea76bac
TopGitSan <c.toparcean@gmail.com>
2019-12-15 11:44:59 +0100

    r-form practice

--- src/app/r-form/r-form.component.ts
+++ src/app/r-form/r-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-r-form',
@@ -6,10 +7,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./r-form.component.css']
 })
 export class RFormComponent implements OnInit {
+  userForm: FormGroup;
+  get name() {
+    return this.userForm.get('name');
+  }
 
+  get hobbies() {
+    return this.userForm.get('hobbies');
+  }
+
+  isChecked: boolean = false;
   constructor() { }
 
   ngOnInit() {
+    this.userForm = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(8)]),
+      username: new FormControl(''),
+      email: new FormControl(''),
+      phone: new FormControl(''),
+      website: new FormControl(''),
+      address: new FormGroup({
+        street: new FormControl(''),
+        suite: new FormControl(''),
+        city: new FormControl(''),
+        zipcode: new FormControl(''),
+        geo: new FormGroup({
+          lat: new FormControl(''),
+          lng: new FormControl(''),
+        })
+      }),
+      company: new FormGroup({
+        name: new FormControl(''),
+        catchphrase: new FormControl(''),
+        bs: new FormControl(''),
+      }),
+      hobbies: new FormArray([])
+    });
+  }
+
+  onAddHobbie(){
+    (this.userForm.get('hobbies') as FormArray).push(
+      new FormGroup({
+        hobbie: new FormControl('', Validators.required),
+        ispec: new FormControl(''),
+        prefered: new FormControl('')
+      })
+    );
+  }
+
+  toggleCheck(status){
+    this.isChecked = !this.isChecked;
+  }
+
+  onDeleteHobbie(index: number){
+    (this.userForm.get('hobbies') as FormArray).removeAt(index);
+  }
+
+  onSubmit(){
+    console.log('Form: ', this.userForm);
   }
 
 }
