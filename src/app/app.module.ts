ab6ec7bfb008003ecc9c97704bf21918dc9c3f75
TopGitSan <c.toparcean@gmail.com>
2020-01-13 22:29:44 +0100

    sign up users

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,6 +26,7 @@ import { RecipeService } from './recipes/recipe.service';
 import { DataStorageService } from './shared/data-storage.service';
 import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';
+import { AuthService } from './auth/auth.service';
 
 @NgModule({
   declarations: [
@@ -47,7 +48,7 @@ import { SigninComponent } from './auth/signin/signin.component';
     SigninComponent
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule, HttpClientModule],
-  providers: [ShoppingListService, RecipeService, DataStorageService],
+  providers: [ShoppingListService, RecipeService, DataStorageService, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
