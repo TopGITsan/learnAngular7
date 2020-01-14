57e2fb565fd374e9e8cdf58f22f919c457446f45
TopGitSan <c.toparcean@gmail.com>
2020-01-14 21:12:36 +0100

    signin a user

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ import { TemplateFormComponent } from './template-form/template-form.component';
 import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 import { RFormComponent } from './r-form/r-form.component';
 import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
 
 const appRoutes: Routes = [
   {path: '', redirectTo: '/recipes' , pathMatch: 'full'},
@@ -23,7 +24,8 @@ const appRoutes: Routes = [
   {path: 'template-form', component: TemplateFormComponent },
   {path: 'reactive-form', component: ReactiveFormComponent },
   {path: 'r-form', component: RFormComponent},
-  {path: 'signup', component: SignupComponent}
+  {path: 'signup', component: SignupComponent},
+  {path: 'signin', component: SigninComponent}
 ];
 
 @NgModule({
