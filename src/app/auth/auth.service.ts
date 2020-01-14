585bb0a2ffadfa8b62b9e7879bb1f156bc99faf3
TopGitSan <c.toparcean@gmail.com>
2020-01-14 21:49:04 +0100

    sending the token

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,8 @@
 import * as firebase from 'firebase';
 
 export class AuthService {
+  token: string;
+
   signupUser(email: string, password: string) {
     firebase.auth().createUserWithEmailAndPassword(email, password)
       .then(response => console.log(response))
@@ -9,7 +11,26 @@ export class AuthService {
 
   signinUser(email: string, password: string) {
     firebase.auth().signInWithEmailAndPassword(email, password)
-      .then(response => console.log('Response: ', response))
+      .then(response => {
+        console.log('Response: ', response);
+        firebase.auth().currentUser.getIdToken()
+          .then(token => {
+            console.log(token);
+            this.token = token;
+          })
+          .catch(error => console.log(error));
+      })
       .catch(error => console.log('Error: ', error));
+  }
+
+  getToken(){
+    firebase.auth().currentUser.getIdToken()
+      .then(token => this.token = token)
+      .catch(error => console.log(error));
+    return this.token;
+  }
+
+  isAuthenticated() {
+    return this.token != null;
   }
 }
