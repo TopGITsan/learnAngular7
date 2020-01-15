41c6a30040958ec2cec13762edfa7bc3dd5ed0f7
TopGitSan <c.toparcean@gmail.com>
2020-01-15 22:12:34 +0100

    route protection and redirection

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,12 @@
 import * as firebase from 'firebase';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
+@Injectable()
 export class AuthService {
   token: string;
+
+  constructor(private router: Router){}
 
   signupUser(email: string, password: string) {
     firebase.auth().createUserWithEmailAndPassword(email, password)
@@ -17,6 +22,7 @@ export class AuthService {
           .then(token => {
             console.log(token);
             this.token = token;
+            this.router.navigate(['/']);
           })
           .catch(error => console.log(error));
       })
