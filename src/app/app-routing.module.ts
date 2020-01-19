619bbc8f3baa57a5cf54c7c26f7ce28d6269d97c
TopGitSan <c.toparcean@gmail.com>
2020-01-19 20:16:26 +0100

    remove unused component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { TemplateFormComponent } from './template-form/template-form.component';
 import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 import { RFormComponent } from './r-form/r-form.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { HomeComponent } from './home/home.component';
 
 const appRoutes: Routes = [
